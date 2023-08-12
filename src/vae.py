8a3cc14ad4e891b9d933f89005506460938bd5c5
Sean Friedowitz <sfriedowitz93@gmail.com>
2023-08-12 13:47:22 -0700

    starting on vae train

--- src/vae.py
+++ src/vae.py
@@ -4,34 +4,39 @@ from torch.nn import functional as F
 
 
 class MolecularVAE(nn.Module):
-    def __init__(self, input_dim: int):
+    def __init__(self, input_dim: int, latent_dim: int = 292):
         super().__init__()
 
-        # The input filter dim should be 35
-        #  corresponds to the size of CHARSET
-        self.conv1 = nn.Conv1d(input_dim, 9, kernel_size=9)
-        self.conv2 = nn.Conv1d(9, 9, kernel_size=9)
-        self.conv3 = nn.Conv1d(9, 10, kernel_size=11)
-        self.fc0 = nn.Linear(940, 435)
-        self.fc11 = nn.Linear(435, 292)
-        self.fc12 = nn.Linear(435, 292)
+        # The input filter dim should correspond to the size of SMILES alphabet
+        self.encoder_conv = nn.Sequential(
+            nn.Conv1d(input_dim, 9, kernel_size=9),
+            nn.ReLU(),
+            nn.Conv1d(9, 9, kernel_size=9),
+            nn.ReLU(),
+            nn.Conv1d(9, 10, kernel_size=11),
+            nn.ReLU(),
+        )
+        self.encoder_embedding = nn.Sequential(
+            nn.Linear(940, 435),
+            nn.SELU(),
+        )
+        self.encoder_mean = nn.Linear(435, latent_dim)
+        self.encoder_logvar = nn.Linear(435, latent_dim)
 
-        self.fc2 = nn.Linear(292, 292)
-        self.gru = nn.GRU(292, 501, 3, batch_first=True)
+        self.fc2 = nn.Linear(latent_dim, latent_dim)
+        self.gru = nn.GRU(latent_dim, 501, 3, batch_first=True)
         self.fc3 = nn.Linear(501, 35)
 
     def encode(self, x):
-        h = F.relu(self.conv1(x))
-        h = F.relu(self.conv2(h))
-        h = F.relu(self.conv3(h))
-        h = h.view(h.size(0), -1)
-        h = F.selu(self.fc0(h))
-        return self.fc11(h), self.fc12(h)
+        x = self.encoding_conv(x)
+        x = x.view(x.size(0), -1)
+        x = self.encoder_embedding(x)
+        return self.encoder_mean(x), self.encoder_conv(x)
 
     def decode(self, z):
         z = F.selu(self.fc2(z))
         z = z.view(z.size(0), 1, z.size(-1)).repeat(1, 120, 1)
-        out, h = self.gru(z)
+        out, _ = self.gru(z)
         out_reshape = out.contiguous().view(-1, out.size(-1))
         y0 = F.softmax(self.fc3(out_reshape), dim=1)
         y = y0.contiguous().view(out.size(0), -1, y0.size(-1))
