bd82a9e6c988bfd4ce982159e4eec30043e83357
Sean Friedowitz <sfriedowitz93@gmail.com>
2023-08-12 23:25:28 -0700

    vae training

--- src/vae.py
+++ src/vae.py
@@ -1,41 +1,35 @@
 import torch
 from torch import nn
-from torch.nn import functional as F
+
+
+# Determined from selfies encoding of QM9 dataset
+# INPUT_SIZE is the longest length of selfies characters
+# INPUT_CHANNELS is the alphabet size of selfies characters
+INPUT_SIZE = 21
+INPUT_CHANNELS = 29
 
 
 class MolecularVAE(nn.Module):
     def __init__(
         self,
         *,
-        input_size: int,
-        input_channels: int,
-        conv_kernels: list[int] = [9, 9, 5],
-        latent_size: int = 292,
-        encoder_hidden_size: int = 435,
-        gru_layers: int = 3,
-        gru_hidden_size: int = 501,
+        latent_size: int = 50,
+        encoder_hidden_size: int = 150,
+        gru_hidden_size: int = 200,
     ):
         super().__init__()
 
-        self.input_size = input_size
-
-        # Per conv layer: output_size = (input_size - kernel_size) / stride + 1
-        conv_output_len = input_size - sum(conv_kernels) + 3
-        if conv_output_len <= 0:
-            raise ValueError("Convolution kernel sizes are greater than input sequence length.")
-        conv_output_size = 9 * conv_output_len
-
         # Encoder layers
-        conv_layers = []
-        conv_layers.append(nn.Conv1d(input_channels, 9, kernel_size=conv_kernels[0]))
-        conv_layers.append(nn.ReLU())
-        for kernel in conv_kernels[1:]:
-            conv_layers.append(nn.Conv1d(9, 9, kernel_size=kernel))
-            conv_layers.append(nn.ReLU())
-
-        self.encoder_conv = nn.Sequential(*conv_layers)
+        self.encoder_conv = nn.Sequential(
+            nn.Conv1d(INPUT_CHANNELS, 9, kernel_size=9, padding=2),
+            nn.ReLU(),
+            nn.Conv1d(9, 9, kernel_size=9, padding=2),
+            nn.ReLU(),
+            nn.Conv1d(9, 10, kernel_size=10, padding=2),
+            nn.ReLU(),
+        )
         self.encoder_linear = nn.Sequential(
-            nn.Linear(conv_output_size, encoder_hidden_size),
+            nn.Linear(80, encoder_hidden_size),
             nn.SELU(),
         )
         self.encoder_mean = nn.Linear(encoder_hidden_size, latent_size)
@@ -46,11 +40,8 @@ class MolecularVAE(nn.Module):
             nn.Linear(latent_size, latent_size),
             nn.SELU(),
         )
-        self.decoder_gru = nn.GRU(latent_size, gru_hidden_size, gru_layers, batch_first=True)
-        self.decoder_output = nn.Sequential(
-            nn.Linear(gru_hidden_size, input_channels),
-            nn.Softmax(dim=1),
-        )
+        self.decoder_gru = nn.GRU(latent_size, gru_hidden_size, 3, batch_first=True)
+        self.decoder_output = nn.Linear(gru_hidden_size, INPUT_CHANNELS)
 
     def n_parameters(self) -> int:
         return sum(torch.numel(x) for x in self.parameters())
@@ -66,7 +57,7 @@ class MolecularVAE(nn.Module):
 
     def decode(self, z):
         z = self.decoder_linear(z)
-        z = z.view(z.size(0), 1, z.size(-1)).repeat(1, self.input_size, 1)
+        z = z.view(z.size(0), 1, z.size(-1)).repeat(1, INPUT_SIZE, 1)
 
         gru, _ = self.decoder_gru(z)
         gru_flat = gru.contiguous().view(-1, gru.size(-1))
@@ -77,7 +68,7 @@ class MolecularVAE(nn.Module):
         return y
 
     def sample(self, z_mean, z_logvar):
-        epsilon = 1e-2 * torch.randn_like(z_logvar)
+        epsilon = torch.randn_like(z_logvar)
         return torch.exp(0.5 * z_logvar) * epsilon + z_mean
 
     def forward(self, x):
