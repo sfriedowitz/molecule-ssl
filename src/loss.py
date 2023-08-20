80a2d7749a0e9fe057157852306953f8dbdb14f8
Sean Friedowitz <sfriedowitz93@gmail.com>
2023-08-19 20:05:38 -0700

    add back linear scheduler

--- src/loss.py
+++ src/loss.py
@@ -5,16 +5,36 @@ import torch.nn.functional as F
 
 
 class VAELoss(nn.Module):
-    def __init__(self, *, kld_scale: float = 1.0, mse_scale: float = 1.0):
+    def __init__(
+        self,
+        *,
+        include_mse: bool = True,
+        beta_max: float = 1.0,
+        beta_start: int = 0,
+        beta_end: int = 0,
+    ):
         super().__init__()
-        self.kld_scale = kld_scale
-        self.mse_scale = mse_scale
+        self.include_mse = include_mse
+
+        # Tracking KL loss contribution
+        self.beta_max = beta_max
+        self.beta_start = beta_start
+        self.beta_end = beta_end
 
         # Cache values from forward pass for tracking
         self.current_ce: Optional[float] = None
         self.current_kld: Optional[float] = None
         self.current_mse: Optional[float] = None
-        self.current_recon: Optional[int] = None
+        self.current_accuracy: Optional[int] = None
+
+    def get_beta(self, epoch):
+        if epoch < self.beta_start:
+            return 0.0
+        elif epoch >= self.beta_end:
+            return self.beta_max
+        else:
+            step_size = self.beta_max / max(self.beta_end - self.beta_start, 1)
+            return step_size * (epoch - self.beta_start)
 
     def forward(
         self,
@@ -24,17 +44,22 @@ class VAELoss(nn.Module):
         y_hat: torch.Tensor,
         z_mean: torch.Tensor,
         z_logvar: torch.Tensor,
+        epoch: int,
     ) -> torch.Tensor:
         # Cross entropy should be computed across one-hot labels,
         # so transpose tensors so labels in dim=1
         ce = F.cross_entropy(xr.transpose(2, 1), x.transpose(2, 1), reduction="sum")
         kld = -0.5 * torch.sum(1.0 + z_logvar - z_mean.pow(2) - z_logvar.exp())
-        mse = F.mse_loss(y_hat, y, reduction="sum")
-        recon = (x.argmax(dim=-1) == xr.argmax(dim=-1)).all(dim=-1).sum()
+        accuracy = (x.argmax(dim=-1) == xr.argmax(dim=-1)).all(dim=-1).sum()
+
+        loss = ce + self.get_beta(epoch) * kld
+        if self.include_mse:
+            mse = F.mse_loss(y_hat, y, reduction="sum")
+            loss += mse
+            self.current_mse = mse.item()
 
         self.current_ce = ce.item()
         self.current_kld = kld.item()
-        self.current_mse = mse.item()
-        self.current_recon = recon.item()
+        self.current_accuracy = accuracy.item()
 
-        return ce + self.kld_scale * kld + self.mse_scale * mse
+        return loss
--- src/train.py
+++ src/train.py
@@ -1,5 +1,6 @@
 import dataclasses
 from dataclasses import dataclass
+from collections import defaultdict
 from typing import Optional
 from loguru import logger
 
@@ -23,9 +24,11 @@ class TrainingConfig:
     gru_hidden_size: int = 500
     mlp_hidden_size: int = 300
     gru_layers: int = 3
-    gru_dropout: float = 0.1
-    mse_scale: float = 1.0
-    kld_scale: float = 1e-3
+    gru_dropout: float = 0.2
+    include_mse: bool = 1.0
+    beta_max: float = 1.0
+    beta_start: int = 0
+    beta_end: int = 0
     lr_init: float = 1e-3
     lr_decay: float = 1.0
     weight_decay: float = 1e-5
@@ -38,23 +41,26 @@ def train_one_epoch(
     optimizer: Optimizer,
     scheduler: Optional[LRScheduler],
     data_loader: DataLoader,
+    epoch: int,
 ):
     model.train()
-
-    metrics = {"ce": 0.0, "kld": 0.0, "mse": 0.0, "accuracy": 0.0}
+    metrics = defaultdict(lambda: 0.0)
     for x, y in data_loader:
         x, y = x.to(device), y.to(device)
         x_recon, y_hat, z_mean, z_logvar = model(x)
 
-        loss = criterion(x, x_recon, y, y_hat, z_mean, z_logvar)
+        loss = criterion(x, x_recon, y, y_hat, z_mean, z_logvar, epoch)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
+        metrics["loss"] += loss.item()
         metrics["ce"] += criterion.current_ce
         metrics["kld"] += criterion.current_kld
-        metrics["mse"] += criterion.current_mse
-        metrics["accuracy"] += criterion.current_recon
+        metrics["accuracy"] += criterion.current_accuracy
+
+        if criterion.current_mse is not None:
+            metrics["mse"] += criterion.current_mse
 
     if scheduler is not None:
         scheduler.step()
@@ -64,25 +70,27 @@ def train_one_epoch(
 
 
 @torch.no_grad()
-def test_one_epoch(model: MolecularVAE, criterion: VAELoss, data_loader: DataLoader):
+def test_one_epoch(model: MolecularVAE, criterion: VAELoss, data_loader: DataLoader, epoch: int):
     model.eval()
-
-    metrics = {"ce": 0.0, "kld": 0.0, "mse": 0.0, "accuracy": 0.0}
+    metrics = defaultdict(lambda: 0.0)
     for x, y in data_loader:
         x, y = x.to(device), y.to(device)
         x_recon, y_hat, z_mean, z_logvar = model(x)
-        _ = criterion(x, x_recon, y, y_hat, z_mean, z_logvar)
+        loss = criterion(x, x_recon, y, y_hat, z_mean, z_logvar, epoch)
 
+        metrics["loss"] += loss.item()
         metrics["ce"] += criterion.current_ce
         metrics["kld"] += criterion.current_kld
-        metrics["mse"] += criterion.current_mse
-        metrics["accuracy"] += criterion.current_recon
+        metrics["accuracy"] += criterion.current_accuracy
+
+        if criterion.current_mse is not None:
+            metrics["mse"] += criterion.current_mse
 
     n = len(data_loader.dataset)
     return {k: v / n for k, v in metrics.items()}
 
 
-def run_training(
+def train_model(
     config: TrainingConfig,
     train_dataset: Dataset,
     test_dataset: Dataset,
@@ -114,28 +122,45 @@ def run_training(
         model = model.to(device)
         logger.info(f"Initializing model with {model.n_parameters()} parameters")
 
-        criterion = VAELoss(kld_scale=config.kld_scale, mse_scale=config.mse_scale)
         optimizer = Adam(model.parameters(), lr=config.lr_init, weight_decay=config.weight_decay)
         scheduler = ExponentialLR(optimizer, gamma=config.lr_decay)
+        criterion = VAELoss(
+            include_mse=config.include_mse,
+            beta_max=config.beta_max,
+            beta_start=config.beta_start,
+            beta_end=config.beta_end,
+        )
 
         train_loader = DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True)
         test_loader = DataLoader(test_dataset, batch_size=config.batch_size, shuffle=True)
 
+        best_test_loss = float("inf")
         for epoch in range(n_epochs):
-            train_metrics = train_one_epoch(model, criterion, optimizer, scheduler, train_loader)
+            train_metrics = train_one_epoch(
+                model, criterion, optimizer, scheduler, train_loader, epoch
+            )
             for metric, value in train_metrics.items():
                 key = f"train_{metric}"
                 mlflow.log_metric(key, value, step=epoch)
 
-            test_metrics = test_one_epoch(model, criterion, test_loader)
+            test_metrics = test_one_epoch(model, criterion, test_loader, epoch)
             for metric, value in test_metrics.items():
                 key = f"test_{metric}"
                 mlflow.log_metric(key, value, step=epoch)
 
+            print(epoch, " ", criterion.get_beta(epoch))
+            mlflow.log_metric("kld_beta", criterion.get_beta(epoch), step=epoch)
+
+            if test_metrics["loss"] < best_test_loss:
+                best_test_loss = test_metrics["loss"]
+                logger.info(f"Achieved best test loss of {best_test_loss:.3f} -- logging model")
+                mlflow.pytorch.log_model(model, "model")
+
             logger.info(
                 f"Epoch {epoch} | "
-                f"Train Accuracy {train_metrics['accuracy']:.4f} | "
-                f"Test Accuracy {test_metrics['accuracy']:.4f} | "
+                f"Train Accuracy {train_metrics['accuracy']:.3f} | "
+                f"Test Accuracy {test_metrics['accuracy']:.3f} | "
+                f"LR {scheduler.get_last_lr()[0]:.3e}"
             )
 
         # Log final model
